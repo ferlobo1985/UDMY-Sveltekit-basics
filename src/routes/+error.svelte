90ef24eceeec03a6b874d96f8a3244b380052e35
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-10-10 01:41:35 -0300

    04-Errors and Linking
    04-Errors and Linking

---
+++ src/routes/+error.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<h1>Sorry, {$page.error?.message}</h1>
+<p>The code is:{$page.status}</p>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,21 @@
 <div class="container">
-    <header>
-        My app header
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <span class="fs-4">My app</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <a href="/" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+          <a href="/contact" class="nav-link">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a href="/users" class="nav-link">Users</a>
+        </li>
+      </ul>
     </header>
-    <hr/>
     <div>
         <slot></slot>
     </div>
