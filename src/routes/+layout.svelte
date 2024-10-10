10222f7fa687a4d3644b0c2343675bd4861f5611
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-10-10 16:39:48 -0300

    12-Active class link
    12-Active class link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,7 @@
+<script>
+  import {page} from '$app/stores';
+</script>
+
 <div class="container">
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
@@ -6,19 +10,26 @@
 
       <ul class="nav nav-pills">
         <li class="nav-item">
-          <a href="/" class="nav-link">Home</a>
+          <a href="/" class="nav-link"
+            class:active={$page.url.pathname === '/'}
+          >Home</a>
         </li>
         <li class="nav-item">
-          <a href="/contact" class="nav-link">Contact</a>
+          <a href="/contact" class="nav-link"
+          class:active={$page.url.pathname === '/contact'}
+          >Contact</a>
         </li>
         <li class="nav-item">
-          <a href="/users" class="nav-link">Users</a>
+          <a href="/users" class="nav-link"
+          class:active={$page.url.pathname === '/users'}
+          >Users</a>
         </li>
         <li class="nav-item">
           <a 
             data-sveltekit-preload-data
             href="/posts" 
             class="nav-link"
+            class:active={$page.url.pathname === '/posts'}
           >Posts</a>
         </li>
       </ul>
