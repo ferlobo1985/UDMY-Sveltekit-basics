8474f3df30f698b1d055d52af11818cb25ab96e9
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-10-10 03:25:55 -0300

    09-Page Server JS
    09-Page Server JS

--- src/routes/users/user/[id]/+page.server.js
+++ src/routes/users/user/[id]/+page.server.js
@@ -1,1 +1,12 @@
-console.log('SERVER ONLY !!')
+import { error } from '@sveltejs/kit';
+
+export async function load({fetch,params}){
+    const url = `https://jsonplaceholder.typicode.com/posts/${params.id}`;
+    const response = await fetch(url);
+    const json = await response.json();
+
+    if(Object.keys(json).length !== 0){
+        return { post: json}
+    }
+    error(404,'Dang it !!')
+}
