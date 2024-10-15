0aa6602991a2fb13a5afc04b1e207dd165ce634f
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-10-15 15:40:23 -0300

    21-SSR and CSR
    21-SSR and CSR

---
+++ src/routes/about/+page.server.js
@@ -0,0 +1,8 @@
+// export const csr = false;
+// export const ssr = true;
+
+export async function load({params}) {
+    return {
+        data:'yes'
+    }
+}
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let data;
+</script>
+
+<h1>Hello</h1>
+<p>{data.data}</p>
