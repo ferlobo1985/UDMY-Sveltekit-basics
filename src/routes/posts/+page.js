590ce35c3be7ce4975306d43dd9427d75c2cb032
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-10-15 15:11:58 -0300

    19-Pre rendering part one
    19-Pre rendering part one

--- src/routes/posts/+page.js
+++ src/routes/posts/+page.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { error } from "@sveltejs/kit";
 import { redirect } from "@sveltejs/kit";
 
+export const prerender = true;
+
 export async function load({params}){
     try{
         const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
